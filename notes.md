d4a6ca2cb03919d3a947a51b3a8ca4fb95e93819
Joseph Martin <josephmartin1350@gmail.com>
2021-10-21 14:05:06 -0600

    time up

---
+++ notes.md
@@ -0,0 +1,18 @@
+base url = http://localhost:3000
+
+also GET(?) /ramens
+also GET(?) /ramens/:id
+
+Feel free to add any additional classes or ids to any elements in the HTML file as you see fit.
+
+    core deliverables
+
+1. see all images in a <div id='ramen-menu'>
+   upon loading request data from the server to get the array of objects that has the ramen info
+   display the image for each ramen using an <img> inside the <div id='ramen-menu'>
+
+2. create an event listener for a click event(I'd rather a mouseover) that displays the ramen info inside
+   <div id="ramen-detail">. it will replace the current text
+
+3. create a new ramen with a submit event using the new ramen form. should be added to 
+   <div id="ramen-menu"> (no persist)
--- src/index.js
+++ src/index.js
@@ -1,1 +1,55 @@
 // write your code here
+const url = `http://localhost:3000`
+const arrUrl = `${url}/ramens`
+const objUrl = `${arrUrl}/:id`
+const ramenMenu = document.getElementById('ramen-menu')
+const ramenDetail = document.getElementById('ramen-detail')
+const makeEl = el => document.createElement(el)
+
+// console.log(url);
+// console.log(arrUrl);
+// console.log(objUrl);
+// all working
+
+
+function renderRamen(ramen) {
+    
+    const img = makeEl('img')
+
+    // do we need to set attributes since they're already named?
+
+    // event listener submit?
+
+    submitForm.addEventListener("submit", () => func(ramen, formInfo)) //what???
+
+    // eventlistener click?
+    showInfo.addEventListener('click', anotherFunc(____)) // idk
+
+
+}
+
+
+// event listener
+// _____.addEventListener("click", function())
+
+// createElement inside the <div id='ramen-menu'>
+// const img = document.createElement('img')
+
+// createElement inside the <div id="ramen-detail">
+// do I?
+
+// pretty sure I need a preventDefault
+e.preventDefault()
+
+// gonna have to append something
+img.append(______)
+___.append(______)
+
+//fetch
+function getRamen() {
+    fetch(url)
+    .then(response => response.json()) //not sure what this actually does
+    .then(arbPaOne => arbPaOne.forEach(ramen => func(ramen)))//idk.. is one pair of these paramaters suposed to be the array?
+}
+
+// this is terrible, but I got further than I expected to
